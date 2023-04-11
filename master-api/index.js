f3ab83ddeeaa3005187f0d3e95f7caab8aa5160e
Muskan Singh <Ms312093@gmail.com>
2023-04-11 18:50:28 +0530

    Updates

--- master-api/index.js
+++ master-api/index.js
@@ -358,17 +358,46 @@ app.put("/approvedCourse/:email", async (req, res) => {
   console.log(userEmail);
   console.log(mycourses);
 
-  let user = await users_collection.findOne({ email: userEmail });
-
-
   try {
     
     let req_course_payment_data = await users_collection.findOneAndUpdate({email : userEmail} ,{ $addToSet: {myCourses:{course_id: mycourses, certificate: false, amount_paid: amount_paid}} } ,{ returnOriginal: false, upsert: true });
     console.log(req_course_payment_data);
 
+    let user = await users_collection.findOne({ email: userEmail });
     // retrieve course package of purchased course
     // let purchasedCourse = await courseDetails.findOne({ _id: mycourses });
     // console.log(purchasedCourse);
+
+    // if (user) {
+    //   let referredByUser = await users_collection.findOne({ referralCode: user.referredByCode });
+    //   if (referredByUser && referredByUser.myCourses.find(course => course.course_id == mycourses)) {
+    //     // ReferredByUser already has the course, but allow referralUser to buy it
+    //     console.log("ReferredByUser already has the course");
+    //     await user.updateOne(
+    //       { $addToSet: { myCourses: { course_id: mycourses, certificate: false, amount_paid: amount_paid } } },
+    //     { returnOriginal: false, upsert: true }
+    //     );
+    //   }
+    //   else {
+    //     // Attempt to update referralUser's myCourses array
+    //     try {
+    //       await user.updateOne(
+    //         { $addToSet: { myCourses: { course_id: mycourses, certificate: false, amount_paid: amount_paid } } },
+    //         { returnOriginal: false, upsert: true }
+    //       );
+    //       console.log("Course added to myCourses array.");
+    //     } catch (error) {
+    //       // Handle duplicate key error
+    //       console.log("Duplicate key error:", error);
+    //     }
+    //   }
+    // }
+    // else {
+    //   // Handle error when user does not exist
+    //   console.log("User does not exist.");
+    // }
+
+
 
     // if (user.referredByCode) {
     //   let commissionPercentage = 0;
